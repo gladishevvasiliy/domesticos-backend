a970f252799f73cdcc62e4d709b7c6caf722d585
Vasiliy.Gladyshev <Vasiliy.Gladyshev@x5.ru>
2020-12-17 17:24:30 +0300

    change url to db after migration

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var mongoose = require ("mongoose");
-var app = express();
+var express = require('express')
+var mongoose = require('mongoose')
+var app = express()
 
-var kruk = require("./routes/kruk.route"); // Imports routes for the kruks
-var composition = require("./routes/composition.route"); // Imports routes for the compositions
+var kruk = require('./routes/kruk.route') // Imports routes for the kruks
+var composition = require('./routes/composition.route') // Imports routes for the compositions
 
-var theport = process.env.PORT || 5000;
+var theport = process.env.PORT || 5000
 
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
+var ___dev_db_url = 'mongodb://domestikos:yaplakal1936@ds137404.mlab.com:37404/kruk'
 var dev_db_url =
-  "mongodb://domestikos:yaplakal1936@ds137404.mlab.com:37404/kruk";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  'mongodb+srv://domestikos:yaplakal1936@kruk.dsm9g.mongodb.net/kruk?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
+mongoose.connect(mongoDB)
+mongoose.Promise = global.Promise
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.use("/kruk", kruk);
-app.use("/composition", composition);
+app.use('/kruk', kruk)
+app.use('/composition', composition)
 
 app.listen(theport, function () {
-    console.log('Backend is working on port', theport);
-});
+  console.log('Backend is working on port', theport)
+})
